c233423cdd7e58259c9f078976111d8ddd9a662d
rohanharikr <rohan@kaarva.com>
2019-11-12 00:11:44 +0530

    added copy tc functionality

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,24 @@ $("#shorter_url").click(function(){
   var shortlink = {
       website_url: $("#hmm").val()
   };
-  console.log(shortlink);
   $.ajax({
       type: "POST",
+    dataType: "json",
       data: JSON.stringify(shortlink),
       url: "https://aa9o8mq5l2.execute-api.ap-south-1.amazonaws.com/Dev/url-shortner",
-      contentType: "application/json"
+      contentType: "application/json",
+      success: function(result) {
+        // console.log(result);
+        $('#short_url').html(result.shortened_url)
+        $('#short_url').removeClass("default");
+        $('#short_url').addClass("new");
+        $('#actone').removeClass("actone");
+        $('#actone').addClass("actonee");
+        $('#acttwo').removeClass("acttwoo");
+        $('#acttwo').addClass("acttwoo");
+      },
+      error: function(result) {
+        alert(result.data);
+      }
+    });
   });
-});
